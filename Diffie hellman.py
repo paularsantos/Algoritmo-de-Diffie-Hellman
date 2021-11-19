befc6021aaeefcf76ee9dcbbcc8cafa3b2e691d7
paularsantos <62888553+paularsantos@users.noreply.github.com>
2021-11-19 16:57:13 -0300

    Add files via upload

---
+++ Diffie hellman.py
@@ -0,0 +1,28 @@
+# ALGORITMO DE DIFFIE-HELLMAN
+# Discente - Paula Santos (201811140027)
+
+NumPrimo = 52
+NumBase = 4
+
+NumSecrAlice = 8
+NumSecrBob = 23
+
+print("VARIÁVEIS COMPARTILHADA PUBLICAMENTE")
+print ("Numero primo: ",NumPrimo)
+print ("Base: ",NumBase)
+
+print (" ----------------------------------------------- \n ")
+print ("TROCA DE CHAVES ENTRE ALICE E BOB")
+A = (NumBase**NumSecrAlice) % NumPrimo
+print ('Alice envia para Bob: ', A)
+
+B = (NumBase ** NumSecrBob) % NumPrimo
+print ("Bob envia para Alice: ", B)
+
+print ("---------------------------------------------------\n")
+print (" CHAVE CALCULADA PRIVADAMENTE")
+ChaveSecrAlice = (B ** NumSecrAlice) % NumPrimo
+print ('Resposta do calculo de Alice: ', ChaveSecrAlice)
+ChaveSecrBob = (A ** NumSecrBob) % NumPrimo
+print ('Resposta do calculo de Bob: ', ChaveSecrBob)
+print ("----------------------------------------------------")
